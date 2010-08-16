59828ef0fec82735a7cafeb43302abf5a420764d
Ubuntums <serhiy@ubuntu.localdomain>
2010-08-16 12:00:46 -0700

    changes design

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,23 +1,11 @@
 class Picture < Asset
 
-  # === List of columns ===
-  #   id                : integer
-  #   data_file_name    : string
-  #   data_content_type : string
-  #   data_file_size    : integer
-  #   assetable_id      : integer
-  #   assetable_type    : string
-  #   type              : string
-  #   locale            : integer
-  #   user_id           : integer
-  #   created_at        : datetime
-  #   updated_at        : datetime
-  # =======================
+ 
 
 
   has_attached_file :data,
-                    :url  => "/system/ckeditor/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/system/ckeditor/pictures/:id/:style_:basename.:extension",
+                    #:url  => "/system/ckeditor/pictures/:id/:style_:basename.:extension",
+                    #:path => ":rails_root/public/system/ckeditor/pictures/:id/:style_:basename.:extension",
                     :styles => { :content => '575>', :thumb => '100x100' }
 
   validates_attachment_size :data, :less_than=>2.megabytes
