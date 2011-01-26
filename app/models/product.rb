80266c9fa56483555d82ae514f92ac12eb9f0164
dallis <serhiy.vm@gmail.com>
2011-01-26 00:43:42 -0800

    newedit

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/pjpeg','image/png','image/x-png','image/gif']
-  attr_accessible :category_id,:sixty, :typ, :price, :sklad, :shortd, :title, :hundredzav, :fortyzav, :ninety, :artukyl, :astma, :eighteenzav, :forty, :twentyzav, :alerg, :thirty, :twenty, :ukrtitle, :seventyzav, :description
+  attr_accessible :category_id,:sixty, :typ, :price, :sklad, :shortd, :title,:photo, :hundredzav, :fortyzav, :ninety, :artukyl, :astma, :eighteenzav, :forty, :twentyzav, :alerg, :thirty, :twenty, :ukrtitle, :seventyzav, :description
   belongs_to :category
   
 #  def self.search(page)#search, page)
