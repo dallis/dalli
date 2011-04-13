e40fce2e91c83af8b8f71ce454629a050cbfc3c6
Ubuntums <serhiy@ubuntu.localdomain>
2011-04-13 05:40:19 -0700

    add bundle

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem "rails", "~> 2.3.5"
+
+gem 'mysql'
+gem 'paperclip'
+gem 'formtastic'
+gem "searchlogic"
+gem 'hoptoad_notifier'
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -30,11 +30,14 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-    config.gem 'paperclip'
-    config.gem 'formtastic'
-    config.gem "searchlogic"
-    config.gem 'hoptoad_notifier'
-  # Only load the plugins named here, in the order given (default is alphabetical).
+    
+  
+  #config.gem 'paperclip'
+  #config.gem 'formtastic'
+  #config.gem "searchlogic"
+  #config.gem 'hoptoad_notifier'
+  
+    # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
